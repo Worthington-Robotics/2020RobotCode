6c2c6760b8a8e2993f8f563e1fea4bd311fad947
PhillipWhiter <34491601+PhillipWhiter@users.noreply.github.com>
2020-01-08 16:52:28 -0500

    Inital commit

---
+++ RobotCode/2020RobotCode/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,58 @@
+package frc.robot;
+public class Constants {
+    /**
+     * device ID declarations ---------------------------------
+     */
+
+    //Talon IDs
+
+    //Spark Ports
+
+    //Solenoid Ports
+
+    /**
+     * Drivetrain tuned values --------------------------------
+     */
+
+    //Physical Constants
+    public static final double DRIVE_WHEEL_TRACK_WIDTH_INCHES = 21.75;
+    public static final double DRIVE_WHEEL_DIAMETER_INCHES = 6.225; // 6
+    public static final double DRIVE_WHEEL_RADIUS_INCHES = DRIVE_WHEEL_DIAMETER_INCHES / 2.0;
+    public static final double TRACK_SCRUB_FACTOR = 1.0;  // TODO tune
+    public static final double ROBOT_LINEAR_INERTIA = 75;  // kg TODO tune
+    public static final double ROBOT_ANGULAR_INERTIA = 10.0;  // kg m^2 TODO tune
+    public static final double ROBOT_ANGULAR_DRAG = 12.0;  // N*m / (rad/sec) TODO tune
+
+    //Path following Constants
+    public static final double ROBOT_MAX_VELOCITY = 120.0; // in/s
+    public static final double ROBOT_MAX_ACCEL = 120.0; // in/s^2
+    public static final double ROBOT_MAX_VOLTAGE = 10.0; // V
+    public static final double Path_Kx = 4.0;  //
+    public static final double PATH_LOOK_AHEAD_TIME = 0.4;  // seconds to look ahead along the path for steering
+    public static final double PATH_MIN_LOOK_AHEAD_DISTANCE = 24.0;  // inches
+
+    //Electrical Constants
+    public static final double DRIVE_V_INTERCEPT = 1.2;  // V //1.6 for practice......................
+    public static final double DRIVE_Kv = 0.316426;  // V per rad/s -.335
+    public static final double DRIVE_Ka = 0.0801;  // V per rad/s^2    0.0801
+    public static final double DRIVE_VCOMP = 10.0; //V
+    public static final double DRIVE_ENCODER_PPR = 4096.0; //encoder counts per revolution
+
+    //PID Constants
+    public static final double ANGLE_KP = 0.04; // 0.065;
+    public static final double ANGLE_KI = 0; // 0.00125;
+    public static final double ANGLE_KD = 0; // 0.1
+    public static final double ANGLE_PID_EPISLON = 1;
+
+    public static final double DRIVE_RIGHT_KP = 1.2;
+    public static final double DRIVE_RIGHT_KI = 0.0;
+    public static final double DRIVE_RIGHT_KD = 25; // 20 for practice bot
+    public static final double DRIVE_RIGHT_KF = 0.53; //.485
+
+    public static final double DRIVE_LEFT_KP = 1.1; // .0885
+    public static final double DRIVE_LEFT_KI = 0.0; //NO INTEGRAL it masks deeper problems
+    public static final double DRIVE_LEFT_KD = 25; //20 for practice
+    public static final double DRIVE_LEFT_KF = 0.53;
+
+
+}
