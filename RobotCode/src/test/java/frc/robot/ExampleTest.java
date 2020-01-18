8a0817ba48661ba967af66e051b639b064c6aa89
coalman321 <coalman321@users.noreply.github.com>
2020-01-17 23:06:30 -0500

    commit example tests

--- RobotCode/src/test/java/frc/robot/ExampleTest.java
+++ RobotCode/src/test/java/frc/robot/ExampleTest.java
@@ -8,9 +8,17 @@ public class ExampleTest {
 
     @Test
     public void testExample(){
-        System.out.println("Testing 1 = 1");
+        System.out.println("Testing for 1 = 1");
+        System.out.println("These print statements will " 
+         + "be captured in the report");
+         System.out.println("They are located on the standard output tab");
         assertEquals(1, 1);
     }
 
+    @Test
+    public void printOnlyTest(){
+        System.out.println("this is a test that only has print statements");
+        System.out.println("there are no assertions here");
+    }
 
 }
