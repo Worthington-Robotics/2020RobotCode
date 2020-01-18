8e4d58a2b6fd0c13256fd9332b2dac2db530f1ad
Toby-Shaw <57643423+Toby-Shaw@users.noreply.github.com>
2020-01-18 11:04:20 -0500

    Added Flywheel class
    Also added shooter

---
+++ RobotCode/src/main/java/frc/robot/subsystems/Shooter/Flywheel.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems.Shooter;
+
+import frc.robot.subsystems.Subsystem;
+
+public class Flywheel extends Subsystem {
+
+    /**
+     * Updates all periodic variables and sensors
+     */
+    @Override
+    public void readPeriodicInputs() {
+
+    }
+
+    /**
+     * Writes the periodic outputs to actuators (motors and ect...)
+     */
+    @Override
+    public void writePeriodicOutputs() {
+
+    }
+
+    /**
+     * Outputs all logging information to the SmartDashboard
+     */
+    @Override
+    public void outputTelemetry() {
+
+    }
+
+    /**
+     * Called to reset and configure the subsystem
+     */
+    @Override
+    public void reset() {
+
+    }
+    public static calcRPM () {
+        
+    }
+    public class PeriodicIO {
+
+    }
+}
