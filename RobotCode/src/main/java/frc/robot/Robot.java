7cb1ed616aa8c7b01f6d5c578105828f65e4a48b
PhillipWhiter <PhillipWhiter@users.noreply.github.com>
2020-01-30 16:10:08 -0500

    Updated Robot Class from Robot test

--- RobotCode/src/main/java/frc/robot/Robot.java
+++ RobotCode/src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,16 +7,14 @@
 
 package frc.robot;
 
+import java.util.Arrays;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.loops.Looper;
 import frc.lib.statemachine.StateMachine;
-import frc.lib.statemachine.StateMachineDescriptor;
-import frc.lib.util.DriveSignal;
-import frc.robot.subsystems.*;
-
-import java.util.Arrays;
+import frc.robot.subsystems.Lights;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,106 +24,127 @@ import java.util.Arrays;
  * project.
  */
 public class Robot extends TimedRobot {
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    private SubsystemManager Manager = new SubsystemManager(Arrays.asList(
-            Drive.getInstance(),
-            PoseEstimator.getInstance()
-    ), false);
-    private Looper EnabledLoops = new Looper();
-    private Looper DisabledLoops = new Looper();
-    private OI Oi = new OI();
+    private SubsystemManager manager;
+    private Looper enabledLooper, disabledLooper;
 
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
     @Override
     public void robotInit() {
-        Manager.registerEnabledLoops(EnabledLoops);
-        Manager.registerDisabledLoops(DisabledLoops);
+        manager = new SubsystemManager(Arrays.asList(
+            //register subsystems here
+            Lights.getInstance()
+        ), true);
 
+        //create the master looper threads
+        enabledLooper = new Looper();
+        disabledLooper = new Looper();
+
+        //register the looper threads to the manager to use for enabled and disabled
+        manager.registerEnabledLoops(enabledLooper);
+        manager.registerDisabledLoops(disabledLooper);
+
+        //add any additional logging sources for capture
+        manager.addLoggingSource(Arrays.asList(
+            StateMachine.getInstance()
+        ));
+
+        // publish the auto list to the dashboard "Auto Selector"
+        SmartDashboard.putStringArray("Auto List", AutoSelector.buildArray()); 
     }
 
+    /**
+     * This function is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
+    @Override
     public void robotPeriodic() {
-        Manager.outputTelemetry();
+        manager.outputTelemetry();
     }
 
     @Override
     public void disabledInit() {
-        // publishes the auto list to the dashboard "Auto Selector"
-        SmartDashboard.putStringArray("Auto List", AutoSelector.buildArray());
+        enabledLooper.stop();
+
+        //Run any reset code here
         StateMachine.getInstance().assertStop();
-        Drive.getInstance().overrideTrajectory(true);
-        //Stop the disabled looper
-        DisabledLoops.stop();
-        //Start the enabled looper
-        EnabledLoops.start();
+
+        disabledLooper.start();
     }
 
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
     @Override
     public void autonomousInit() {
-        //Stop the disabled looper
-        DisabledLoops.stop();
-        //Reset all important subsystems
-        PoseEstimator.getInstance().reset();
-        Drive.getInstance().reset();
+        disabledLooper.stop();
 
-        //Start the enabled looper
-        EnabledLoops.start();
+        //reset anything here
+
+        enabledLooper.start();
+
+        String[] autoList = AutoSelector.buildArray();
 
         //pulls auto selector from labview DB
-        final String[] autoList = AutoSelector.buildArray();
-        //Default to last entry if Dashboard not found
-        final String autoSelected = SmartDashboard.getString("Auto Selector", autoList[autoList.length - 1]);
-        //get selected auto as a state machine descriptor
-        final StateMachineDescriptor auto = AutoSelector.autoSelect(autoSelected);
-        //perform a null check on the auto to see if it is valid
-        if (auto != null) StateMachine.getInstance().runMachine(auto);
+        String autoSelected = SmartDashboard.getString("Auto Selector", autoList[autoList.length - 1]);
+
+        //schedule the state machine to run the selected autonomous
+        StateMachine.getInstance().runMachine(AutoSelector.autoSelect(autoSelected));
     }
 
+    /**
+     * This function is called periodically during autonomous.
+     */
     @Override
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
+        
     }
 
     @Override
     public void teleopInit() {
-        //Stop the disabled looper
-        DisabledLoops.stop();
+        disabledLooper.stop();
 
-        //TODO INVESTIGATE but likely a bad thing to do
-        //May want to remove these given that the robot was likely already running. may cause issues on field mode switch
-        //PoseEstimator.getInstance().reset();
-        //Drive.getInstance().reset();
+        //reset anything here
 
-        //Start the enabled looper
-        EnabledLoops.start();
-
-        //for saftey reasons switch drivetrain into open loop forcibly
-        Drive.getInstance().setOpenLoop(DriveSignal.NEUTRAL);
+        enabledLooper.start();
     }
 
+    /**
+     * This function is called periodically during operator control.
+     */
     @Override
     public void teleopPeriodic() {
-        Scheduler.getInstance().run();
+
     }
 
     @Override
     public void testInit() {
-        //Stop the disabled looper
-        DisabledLoops.stop();
+        disabledLooper.stop();
 
-        //May want to remove these given that the robot was likely already running. may cause issues on field mode switch
-        PoseEstimator.getInstance().reset();
-        Drive.getInstance().reset();
+        //reset anything here
 
-        //Start the enabled looper
-        EnabledLoops.start();
-
+        enabledLooper.start();
     }
 
+    /**
+     * This function is called periodically during test mode.
+     */
     @Override
     public void testPeriodic() {
-        Scheduler.getInstance().run();
+        
     }
-
 }
