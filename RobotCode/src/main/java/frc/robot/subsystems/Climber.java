0e6d6b978807ae9fa63c1353cf19d6d3c45bc48a
Tazmania40 <tzupfer@gmail.com>
2020-01-31 17:59:27 -0500

    Working on Climber
    See Issue #13

--- RobotCode/src/main/java/frc/robot/subsystems/Climber.java
+++ RobotCode/src/main/java/frc/robot/subsystems/Climber.java
@@ -1,16 +1,41 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class Climber extends Subsystem {
+    public DoubleSolenoid unfoldSolenoid, extendSolenoid;
+    public boolean unfoldCurrentState, unfoldIntendedState, extendCurrentState, extendIntendedState;
+    public boolean climberUp;
+
+    public Climber() {
+        unfoldSolenoid = new DoubleSolenoid(2, 3);
+        extendSolenoid = new DoubleSolenoid(4, 5);
+    }
+    public Climber mClimber = new Climber();
+    public Climber getInstance() {return mClimber;}
+
+    
     @Override
     public void readPeriodicInputs() {
 
+        if (unfoldSolenoid.get() == Value.kForward) {
+            unfoldCurrentState = true;
+        } else if (unfoldSolenoid.get() == Value.kReverse) {
+            unfoldCurrentState = false;
+        }
+        if (extendSolenoid.get() == Value.kForward) {
+            extendCurrentState = true;
+        } else if (extendSolenoid.get() == Value.kReverse) {
+            extendCurrentState = false;
+        }
     }
 
     @Override
     public void writePeriodicOutputs() {
+        if (unfoldCurrentState != unfoldIntendedState) {
 
+        }
     }
 
     @Override
@@ -23,15 +48,11 @@ public class Climber extends Subsystem {
 
     }
 
-    public DoubleSolenoid unfoldsolenoid, extendsolenoid;
+    public void setUnfold(Value open) {
 
-    public Climber() {
-        unfoldsolenoid = new DoubleSolenoid(2, 3);
-        extendsolenoid = new DoubleSolenoid(4, 5);
     }
 
-
-    public class periodicio extends Subsystem.PeriodicIO {
+    public class periodicIO extends Subsystem.PeriodicIO {
         public DoubleSolenoid.Value unfoldsolenoidinput = DoubleSolenoid.Value.kOff;
         public DoubleSolenoid.Value extendsolenoidinput = DoubleSolenoid.Value.kOff;
     }
