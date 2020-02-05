5d00275e6924fe94a13014c19b323c1ad09f9da2
coalman321 <coalman321@users.noreply.github.com>
2020-02-04 19:21:43 -0500

    add null checking to state machine

--- RobotCode/src/main/java/frc/lib/statemachine/StateMachine.java
+++ RobotCode/src/main/java/frc/lib/statemachine/StateMachine.java
@@ -1,5 +1,7 @@
 package frc.lib.statemachine;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.util.Logable;
@@ -22,6 +24,7 @@ public class StateMachine implements Logable{
     private final LoggingData data = new LoggingData();
     private final double delay = 0.020;
     private volatile StateMachineDescriptor descriptor;
+    private Notifier thread;
 
     /**
      * prevent the creation of a new state machine object
@@ -35,42 +38,44 @@ public class StateMachine implements Logable{
      * The machine handles the starting code, running each state for a specified amount of time
      * (unless it ends early) and then advancing to the next state untill complete. 
      */
-    private final Runnable Man = () -> {
+    private final Runnable stateMachine = () -> {
         try {
             System.out.println("State machine starting execution");
+
+            //validate the descriptor before use
+            if(handleNull(descriptor, true)) return;
 
             //run the onStart code from the descriptor
             descriptor.onStart();
 
-            //state goes to 0 to start
+            //validate the queue before use
             ConcurrentLinkedQueue<ActionGroup> queuedStates = descriptor.getStates();
+            if(handleNull(queuedStates, false)) return;
+
+            //state goes to 0 to start
             data.state.set(0);
             SmartDashboard.putNumber("StateMachine/state", data.state.get());
-            if (queuedStates == null) {
-                //a null state queue has been passed
-                //need to set conditions and exit major loop
-                data.state.set(-2);
+            
+
+            while (!queuedStates.isEmpty() && !data.wantStop.get()) {
+                //pull the next element from the queue and run the state
                 SmartDashboard.putNumber("StateMachine/state", data.state.get());
-            } else {
-                while (!queuedStates.isEmpty() && !data.wantStop.get()) {
-                    //pull the next element from the queue and run the state
-                    SmartDashboard.putNumber("StateMachine/state", data.state.get());
-                    data.currentState = queuedStates.poll();
-                    data.currentState.onStart();
+                data.currentState = queuedStates.poll();
+                data.currentState.onStart();
 
-                    //wait for the state to complete exectuing
-                    while (!data.currentState.isFinished() && !data.wantStop.get()) {
-                        data.t_start = Timer.getFPGATimestamp();
-                        data.currentState.onLoop();
-                        Timer.delay(delay - (Timer.getFPGATimestamp() - data.t_start));
-                    }
-
-                    //when complete stop the state and increment the state counter
-                    data.currentState.onStop();
-                    data.state.getAndAdd(1);
+                //wait for the state to complete exectuing
+                while (!data.currentState.isFinished() && !data.wantStop.get()) {
+                    data.t_start = Timer.getFPGATimestamp();
+                    data.currentState.onLoop();
+                    Timer.delay(delay - (Timer.getFPGATimestamp() - data.t_start));
                 }
+
+                //when complete stop the state and increment the state counter
+                data.currentState.onStop();
+                data.state.getAndAdd(1);
             }
             data.state.set(-1);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             data.state.set(-3);
@@ -81,10 +86,27 @@ public class StateMachine implements Logable{
             data.wantStop.set(true);
 
             //run the onStop code from the descriptor
-            descriptor.onStop();
+            //validate the descriptor before use
+            if(!handleNull(descriptor, true)) descriptor.onStop();
             System.out.println("State machine execution complete");
         }
     };
+
+    private boolean handleNull(Object o, boolean isDescriptor){
+        if(o == null){
+            data.state.set(-2);
+            if(isDescriptor){
+                System.out.println("State machine was passed a null descriptor");
+                DriverStation.reportWarning("State machine was passed a null descriptor", false);
+            } else {
+                System.out.println("State machine was passed a null state queue");
+                DriverStation.reportWarning("State machine was passed a null state queue", false);
+            }
+            return true;
+        }
+
+        return false;
+    }
 
     /**
      * Starts the state machine (if not already runnning a descriptor)
@@ -106,9 +128,13 @@ public class StateMachine implements Logable{
         //move the new state machine in
         descriptor = descrip;
 
+        //validate the descriptor before use
+        if(handleNull(descriptor, true)) return false;
+
         //start the new state machine thread
-        Thread thread = new Thread(Man);
-        thread.start();
+        thread = new Notifier(stateMachine);
+        thread.setName("State Machine");
+        thread.startSingle(0.0);
 
         return true;
     }
@@ -126,9 +152,11 @@ public class StateMachine implements Logable{
      * Forces the state machine to stop and exit within the next iteration.
      */
     public void assertStop() {
-        if (!data.wantStop.get()) {
+        if (!data.wantStop.get() && thread != null) {
             data.wantStop.set(true);
             System.out.println("State Machine Halting");
+            thread.stop();
+            thread.close();
         }
     }
 
