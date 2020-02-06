959b42f85d7931cfc9cf3d508a5e5503cf57be54
Mark Fong <markcfong561@gmail.com>
2020-02-06 16:11:50 -0500

    I'm so cool

--- RobotCode/src/main/java/frc/robot/subsystems/ColorWheel.java
+++ RobotCode/src/main/java/frc/robot/subsystems/ColorWheel.java
@@ -5,19 +5,26 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.drivers.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import frc.robot.Constants;
 import frc.lib.util.Util;
-import java.awt.Color;
+import edu.wpi.first.wpilibj.util.Color;
 import static frc.lib.drivers.ColorSensorV3.*;
 
 public class ColorWheel extends Subsystem {
 
-    private TalonSRX colorWheelTalon;
+    private final TalonSRX colorWheelTalon;
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 colorSensor;
-    private final char[] wheelColors = new char[]{'B', 'Y', 'R', 'G'};
-    private String wheelColorsOrder = new String(wheelColors);
+    private final char[] wheelColors = new char[] { 'B', 'Y', 'R', 'G' };
+    private final String wheelColorsOrder = new String(wheelColors);
+    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
     private ColorWheel() {
         colorWheelTalon = new TalonSRX(Constants.COLOR_WHEEL);
@@ -38,30 +45,34 @@ public class ColorWheel extends Subsystem {
         String gameData;
         gameData = DriverStation.getInstance().getGameSpecificMessage();
         if (gameData.length() > 0) {
-            periodic.fmsColor = colorConvert(gameData.charAt(0));
+            periodic.fms_color = colorConvert(gameData.charAt(0));
         } else {
-            periodic.fmsColor = 'U';
+            periodic.fms_color = 'U';
         }
-        periodic.closedLoopError = colorWheelTalon.getClosedLoopError();
+        periodic.close_loop_error = colorWheelTalon.getClosedLoopError();
+        periodic.RGB = new double[] { periodic.detected_color.red, periodic.detected_color.blue, periodic.detected_color.green };
+        periodic.detected_color = colorSensor.getColor();
     }
 
     @Override
     public void writePeriodicOutputs() {
-        periodic.RGB = new int[]{colorSensor.getRed(), colorSensor.getBlue(), colorSensor.getGreen()};
-        if (!periodic.colorMotorPidOn) {
+        if (!periodic.color_motor_pid_on) {
             colorWheelTalon.set(ControlMode.Position, inchesToTicks(Constants.COLOR_WHEEL_ROTATION_DISTANCE));
         } else {
             colorWheelTalon.set(ControlMode.Position, inchesToTicks(periodic.distance));
         }
     }
 
-    private double inchesToTicks(double inches) {
+    private double inchesToTicks(final double inches) {
         return (inches / (Constants.COLOR_WHEEL_SPINNER_DIA * Math.PI)) / Constants.ENCODER_5046_CPR;
     }
 
     @Override
     public void outputTelemetry() {
-
+        SmartDashboard.putNumber("Color Wheel/Red", periodic.detected_color.red);
+        SmartDashboard.putNumber("Color Wheel/Blue", periodic.detected_color.blue);
+        SmartDashboard.putNumber("Color Wheel/Green", periodic.detected_color.green);
+        SmartDashboard.putString("Color Wheel/Detected Color", "" + cDetected());
     }
 
     /**
@@ -73,32 +84,30 @@ public class ColorWheel extends Subsystem {
         return colorFromRGB(periodic.RGB);
     }
 
-    public static char colorFromRGB(int[] RGB){
-        //RGB Values: Blue: 0, 255, 255. Green: 0, 255, 0. Red: 255, 0, 0. Yellow: 255, 255, 0.
-        //H Values: Blue: 180. Green: 120. Yellow: 60. Red: 0
+    public static char colorFromRGB(final double[] RGB) {
+        // RGB Values: Blue: 0, 255, 255. Green: 0, 255, 0. Red: 255, 0, 0. Yellow: 255,
+        // 255, 0.
+        // H Values: Blue: 180. Green: 120. Yellow: 60. Red: 0
 
-        float[] hsv = new float[3];
-        Color.RGBtoHSB(RGB[0], RGB[1], RGB[2], hsv);
-        //System.out.println(hsv[0] + ", " + hsv[1] + ", " + hsv[2]);
+        final float[] hsv = new float[3];
+        java.awt.Color.RGBtoHSB((int) RGB[0] * 255, (int) RGB[1] * 255, (int) RGB[2] * 255, hsv);
+        // System.out.println(hsv[0] + ", " + hsv[1] + ", " + hsv[2]);
         hsv[0] *= 360;
         hsv[1] *= 100;
         hsv[2] *= 100;
-        //System.out.println(hsv[0] + ", " + hsv[1] + ", " + hsv[2]);
+        // System.out.println(hsv[0] + ", " + hsv[1] + ", " + hsv[2]);
 
-        if(hsv[2] < Constants.COLOR_WHEEL_VAL_LIMIT || hsv[1] < Constants.COLOR_WHEEL_SAT_LIMIT) {
+        if (hsv[2] < Constants.COLOR_WHEEL_VAL_LIMIT || hsv[1] < Constants.COLOR_WHEEL_SAT_LIMIT) {
             return 'U';
-        }
-        else {
-            if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_RED_HUE1, Constants.COLOR_WHEEL_HUE_ERROR) || Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_RED_HUE2, Constants.COLOR_WHEEL_HUE_ERROR)) {
+        } else {
+            if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_RED_HUE1, Constants.COLOR_WHEEL_HUE_ERROR)
+                    || Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_RED_HUE2, Constants.COLOR_WHEEL_HUE_ERROR)) {
                 return 'R';
-            }
-            else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_YELLOW_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
+            } else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_YELLOW_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
                 return 'Y';
-            }
-            else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_GREEN_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
+            } else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_GREEN_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
                 return 'G';
-            }
-            else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_BLUE_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
+            } else if (Util.epsilonEquals(hsv[0], Constants.COLOR_WHEEL_BLUE_HUE, Constants.COLOR_WHEEL_HUE_ERROR)) {
                 return 'B';
             } else {
                 return 'U';
@@ -107,67 +116,71 @@ public class ColorWheel extends Subsystem {
     }
 
     /**
-     * Takes colors and converts it from what the robot sees
-     * to what the field sees or visa versa
+     * Takes colors and converts it from what the robot sees to what the field sees
+     * or visa versa
      *
-     * @param color Color Red, Yellow, Green, Blue that the robot is seeing
-     *              or what the field is seeing.
-     * @return Returns the color that the field sensor is sees
-     * or what the robot sees
+     * @param color Color Red, Yellow, Green, Blue that the robot is seeing or what
+     *              the field is seeing.
+     * @return Returns the color that the field sensor is sees or what the robot
+     *         sees
      */
-    private static char colorConvert(char color) {
+    private static char colorConvert(final char color) {
         switch (color) {
-            case 'R':
-                return 'B';
-            case 'B':
-                return 'R';
-            case 'Y':
-                return 'G';
-            case 'G':
-                return 'Y';
-            default:
-                return 'U';
+        case 'R':
+            return 'B';
+        case 'B':
+            return 'R';
+        case 'Y':
+            return 'G';
+        case 'G':
+            return 'Y';
+        default:
+            return 'U';
         }
     }
 
     /**
-     * Picks the direction the wheel has to spin for maximum efficiency and calculates distance to spin the control panel
+     * Picks the direction the wheel has to spin for maximum efficiency and
+     * calculates distance to spin the control panel
      *
      */
     private void distance() {
-        periodic.colorDirectionCalc = wheelColorsOrder.indexOf(colorConvert(periodic.fmsColor)) - wheelColorsOrder.indexOf(cDetected());
-        if (wheelColorsOrder.indexOf(colorConvert(periodic.fmsColor)) == -1 || wheelColorsOrder.indexOf(cDetected()) == -1) {
+        periodic.color_direction_calc = wheelColorsOrder.indexOf(colorConvert(periodic.fms_color))
+                - wheelColorsOrder.indexOf(cDetected());
+        if (wheelColorsOrder.indexOf(colorConvert(periodic.fms_color)) == -1
+                || wheelColorsOrder.indexOf(cDetected()) == -1) {
             periodic.distance = 0;
-        } else if (periodic.colorDirectionCalc == -3) {
+        } else if (periodic.color_direction_calc == -3) {
             periodic.distance = -12.5;
-        }
-        else if (periodic.colorDirectionCalc == 3) {
+        } else if (periodic.color_direction_calc == 3) {
             periodic.distance = 12.5;
         } else {
-            periodic.distance = periodic.colorDirectionCalc * 12.5;
+            periodic.distance = periodic.color_direction_calc * 12.5;
         }
     }
 
     @Override
     public void reset() {
         periodic = new ColorWheelIO();
-        colorSensor.configureColorSensor(ColorSensorResolution.kColorSensorRes18bit, ColorSensorMeasurementRate.kColorRate100ms, GainFactor.kGain1x);
+        colorSensor.configureColorSensor(ColorSensorResolution.kColorSensorRes18bit,
+                ColorSensorMeasurementRate.kColorRate100ms, GainFactor.kGain1x);
         configTalon();
     }
-    public void setDemand(double newDemand) {
+
+    public void setDemand(final double newDemand) {
         periodic.demand = newDemand;
     }
 
     public boolean isOnTarget() {
-        return Util.epsilonEquals(periodic.closedLoopError, 0, 20);
+        return Util.epsilonEquals(periodic.close_loop_error, 0, 20);
     }
 
     public boolean checkColor() {
-        return periodic.fmsColor == cDetected();
+        return periodic.fms_color == cDetected();
     }
 
-    public void setColorMotorPidOn(boolean motorOn) {
-        periodic.colorMotorPidOn = motorOn;
+    public void setColorMotorPidOn(final boolean motorOn) {
+        periodic.color_motor_pid_on = motorOn;
     }
 
     private void configTalon() {
@@ -185,12 +198,13 @@ public class ColorWheel extends Subsystem {
     }
 
     public class ColorWheelIO extends Subsystem.PeriodicIO {
-        public double closedLoopError = 0;
-        public char fmsColor = 'U';
+        public double close_loop_error = 0;
+        public char fms_color = 'U';
         public double distance = 0;
-        public int[] RGB = new int[]{0, 0, 0};
-        public int colorDirectionCalc;
+        public double[] RGB = new double[] { 0, 0, 0 };
+        public int color_direction_calc;
         public double demand = 0.0;
-        public boolean colorMotorPidOn = false;
+        public boolean color_motor_pid_on = false;
+        public Color detected_color;
     }
 }
