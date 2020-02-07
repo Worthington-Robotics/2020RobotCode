dc9c7cddf797b139bc3753bfe381f9f6b32d5031
Tazmania40 <tzupfer@gmail.com>
2020-02-07 16:05:48 -0500

    Making Actions for Color Wheel

---
+++ RobotCode/src/main/java/frc/robot/actions/colorwheelactions/ColorWheelPosition.java
@@ -0,0 +1,27 @@
+package frc.robot.actions.colorwheelactions;
+
+import frc.lib.statemachine.Action;
+
+public class ColorWheelPosition extends Action {
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+}
