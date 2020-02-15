4972bba55f5c40741a8b8b4d0a025c417d4c5f16
coalman321 <coalman321@users.noreply.github.com>
2020-02-15 10:06:06 -0500

    brooming out the faf

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -12,8 +12,8 @@ public class Constants {
     
     //Pigion ID
     public static final int PIGION_ID = 0;
-    //Talon SRX IDs
-    //Talon FX IDs
+
+    //Talon SRX & FX IDs
     public static final int DRIVE_FRONT_RIGHT_ID = 1;
     public static final int DRIVE_MIDDLE_RIGHT_ID = 2;
     public static final int DRIVE_BACK_RIGHT_ID = 3;
@@ -43,6 +43,7 @@ public class Constants {
     
     public static final int INTAKE_LOW_ID = 3;
     public static final int INTAKE_HIGH_ID = 4;
+
     //Color Sensor Port
     public static final int COLOR_SENSOR_PORT = 0;
     
@@ -57,57 +58,45 @@ public class Constants {
     public static final HIDHelper.HIDConstants SECOND_STICK = new HIDHelper.HIDConstants(SECOND, 0.1, 0.99, 0.99, 0.8, 2);
 
     /*
-        Flywheel Tuned Values
+        Flywheel Tuned Values ---------------------------------
     */
     public static final double MAX_RPM = 6000;
     public static final double IDLE_RPM = 4000;
     public static final double RPM_ACCEPTIBLE_ERROR = 300; // Ticks per 100 ms
 
     /* 
-        Turret tuned values ---------------
+        Turret tuned values -----------------------------------
     */
     public static final double TURRET_MAX_SPEED = .75;
     public static final double TURRET_CONTROL_PID_P = 0;
     public static final double TURRET_CONTROL_PID_D = 0;
     public static final double TURRET_DEGREES_TO_TICKS = 85.26;
     /**
-     * Drivetrain tuned values --------------------------------
+     *   Drivetrain tuned values ------------------------------
      */
 
-    //DEBUG AND TESTING
+    //DEBUG AND TESTING flags
     public static final boolean DEBUG = false;
-    //Encoder Constants
-    public static final double ENCODER_5046_CPR = 1024;
-
-    //Path Following Constants
-    public static final double PATH_FOLLOWING_LOOKAHEAD = 0;
-    public static final double DRIVETRAIN_UPDATE_RATE = 0;
-    public static final double PATH_FOLLOWING_MAX_ACCELERATION = 0;
-    public static final double DRIVE_MAX_VEL = 0; //in/s
-    //Test Flags 
     public static final boolean RAMPUP = false;
+    public static final boolean ENABLE_MP_TEST_MODE = false;
     public static final double MP_TEST_SPEED = 72; //in/s
+
+    public static double LOOPER_DT = 0.01;    
+    
     //Physical Constants
     public static final double DRIVE_WHEEL_TRACK_WIDTH_INCHES = 21.75;
     public static final double DRIVE_WHEEL_DIAMETER_INCHES = 6.225; // 6
     public static final double DRIVE_WHEEL_RADIUS_INCHES = DRIVE_WHEEL_DIAMETER_INCHES / 2.0;
     public static final double TRACK_SCRUB_FACTOR = 1.0;  // TODO tune
-    public static final double ROBOT_LINEAR_INERTIA = 75;  // kg TODO tune
-    public static final double ROBOT_ANGULAR_INERTIA = 10.0;  // kg m^2 TODO tune
-    public static final double ROBOT_ANGULAR_DRAG = 12.0;  // N*m / (rad/sec) TODO tune
 
     //Path following Constants
     public static final double ROBOT_MAX_VELOCITY = 120.0; // in/s
-    public static final double ROBOT_MAX_ACCEL = 120.0; // in/s^2
-    public static final double ROBOT_MAX_VOLTAGE = 10.0; // V
-    public static final double Path_Kx = 4.0;  //
-    public static final double PATH_LOOK_AHEAD_TIME = 0.4;  // seconds to look ahead along the path for steering
-    public static final double PATH_MIN_LOOK_AHEAD_DISTANCE = 24.0;  // inches
+    public static final double ROBOT_MAX_ACCEL = 60.0; // in/s^2
+    public static final double PATH_FOLLOWING_LOOKAHEAD = 0;
+    public static final double DRIVETRAIN_UPDATE_RATE = LOOPER_DT;
+    public static final double PATH_FOLLOWING_MAX_ACCELERATION = 0;
 
     //Electrical Constants
-    public static final double DRIVE_V_INTERCEPT = 1.2;  // V //1.6 for practice......................
-    public static final double DRIVE_Kv = 0.316426;  // V per rad/s -.335
-    public static final double DRIVE_Ka = 0.0801;  // V per rad/s^2    0.0801
     public static final double DRIVE_VCOMP = 10.0; //V
     public static final double DRIVE_ENCODER_PPR = 4096.0 * 3.68; //encoder counts per revolution * gear ratio
 
@@ -126,26 +115,17 @@ public class Constants {
     public static final double DRIVE_LEFT_KI = 0.0; //NO INTEGRAL it masks deeper problems
     public static final double DRIVE_LEFT_KD = 25; //20 for practice
     public static final double DRIVE_LEFT_KF = 0.53;
-	public static final double kPathFollowingMaxAccel = 0;
-	public static final boolean ENABLE_MP_TEST_MODE = false;
-
-	public static double LOOPER_DT = 0.01;
 
 	public static double LEFTFLYWHEELFALCON_KD = 0.0;
     public static double LEFTFLYWHEELFALCON_KP = 0.0;
     public static double RIGHTFLYWHEELFALCON_KD = 0.0;
     public static double RIGHTFLYWHEELFALCON_KP = 0.0;
 
-	//Color Wheel Constants
-    public static final int COLOR_WHEEL_RED_HUE1 = 0;
-    public static final int COLOR_WHEEL_RED_HUE2 = 360;
-    public static final int COLOR_WHEEL_YELLOW_HUE = 60;
-    public static final int COLOR_WHEEL_GREEN_HUE = 120;
-    public static final int COLOR_WHEEL_BLUE_HUE = 180;
-    public static final int COLOR_WHEEL_HUE_ERROR = 10;
-
-    public static final int COLOR_WHEEL_SAT_LIMIT = 80;
-    public static final int COLOR_WHEEL_VAL_LIMIT = 80;
+    /**
+     * Color Wheel Constants ---------------------------
+     */
+    //encoder constants
+    public static final double COLOR_ENCODER_CPR = 1024;
 
     public static final double COLOR_WHEEL_ROTATION_DISTANCE = 3.2 * (16 * Math.PI);
 
