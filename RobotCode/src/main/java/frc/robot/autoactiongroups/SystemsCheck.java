cbda6a2df2514cc519baa3a05dc6b5d4c949bf5d
PhillipWhiter <PhillipWhiter@users.noreply.github.com>
2020-03-05 16:21:17 -0500

    Update SystemsCheck.java

--- RobotCode/src/main/java/frc/robot/autoactiongroups/SystemsCheck.java
+++ RobotCode/src/main/java/frc/robot/autoactiongroups/SystemsCheck.java
@@ -21,8 +21,8 @@ public class SystemsCheck extends StateMachineDescriptor {
     public SystemsCheck() {
         addParallel(new Action[] {new DummyDrive(true), new IntakeAction(), new LightsStateTest(1)}, 5000);
         addParallel(new Action[] {new UnfoldAction(), new ClimbUpAction(), new DummyDrive(true), new Shift(), new LightsStateTest(2)}, 5000);
-        addParallel(new Action[] {new ArmAction(), new colorWheelManual(false), new LightsStateTest(3)}, 5000);
-        addParallel(new Action[] {new ClimbDownAction(), new FoldAction(), new colorWheelManual(true), new LightsStateTest(4)}, 5000);
+        addParallel(new Action[] {new ArmAction(), new LightsStateTest(3)}, 5000);
+        addParallel(new Action[] {new ClimbDownAction(), new FoldAction(), new LightsStateTest(4)}, 5000);
         addParallel(new Action[] {new SetManualFlywheel(), new Recenter(0), new ArmActionUp(), new LightsStateTest(5)}, 5000);  
         addParallel(new Action[] {new ShootAction(), new LightsStateTest(6)}, 5000);
         addParallel(new Action[] {new SuperstructureDisable(), new LightsStateTest(7)}, 5000);
