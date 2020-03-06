03c9abcc71ac4dfda7d720a6a18901ee6d6bb62d
markcfong561 <35080862+markcfong561@users.noreply.github.com>
2020-03-06 16:37:59 -0500

    Fixing and Creating new autos to be tested
    Co-Authored-By: tazmania40 <tazmania40@users.noreply.github.com>

--- RobotCode/src/main/java/frc/robot/autoactiongroups/stealTwoShootSeven.java
+++ RobotCode/src/main/java/frc/robot/autoactiongroups/stealTwoShootSeven.java
@@ -11,20 +11,30 @@ import frc.robot.actions.shooteraction.TurretPIDControl;
 import frc.robot.actions.superaction.IntakeAction;
 import frc.robot.actions.superaction.ShootAction;
 
-public class stealingTwoFromEnemy extends StateMachineDescriptor {
-    public stealingTwoFromEnemy() {
-        //Waypoint Setup
+public class stealTwoShootSeven extends StateMachineDescriptor {
+    public stealTwoShootSeven() {
         ArrayList<Waypoint> waypoints = new ArrayList<>();
         ArrayList<Waypoint> waypoints2 = new ArrayList<>();
+        ArrayList<Waypoint> waypoints3 = new ArrayList<>();
+        ArrayList<Waypoint> waypoints4 = new ArrayList<>();
         waypoints.add(new Waypoint(new Translation2d(0,0), 72));
         waypoints.add(new Waypoint(new Translation2d(125, 0), 72));
         Path stealingPath1 = new Path(waypoints);
         waypoints2.add(new Waypoint(new Translation2d(125, 0), 72));
         waypoints2.add(new Waypoint(new Translation2d(90, 130), 72));
-        waypoints2.add(new Waypoint(new Translation2d(92, 125), 50));
+        waypoints2.add(new Waypoint(new Translation2d(85, 125), 50));
         Path stealingPath2 = new Path(waypoints2);
+        waypoints3.add(new Waypoint(new Translation2d(85, 125), 50));
+        waypoints3.add(new Waypoint(new Translation2d(92, 125), 72));
+        Path stealingPath3 = new Path(waypoints3);
+        waypoints4.add(new Waypoint(new Translation2d(92, 125), 72));
+        waypoints4.add(new Waypoint(new Translation2d(45, 130), 72));
+        Path stealingPath4 = new Path(waypoints4);
+
         //Actual Auto
         addParallel(new Action[]{new DriveTra(stealingPath1, false), new IntakeAction(), new TurretPIDControl()}, 5000);
         addParallel(new Action[]{new DriveTra(stealingPath2, true), new ShootAction(), new TurretPIDControl()}, 8000);
+        addParallel(new Action[]{new DriveTra(stealingPath3, false), new IntakeAction(), new TurretPIDControl()}, 3000);
+        addParallel(new Action[]{new DriveTra(stealingPath4, true), new ShootAction(), new TurretPIDControl()}, 3000);
     }
 }
